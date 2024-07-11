aa0a67a445217d1183ca132644318010ded8473f
simtalk <55088988+simtalk@users.noreply.github.com>
2024-07-11 08:54:32 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,7 +1,7 @@
-export const OWNER = "Licoy";
-export const REPO = "ChatGPT-Midjourney";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const OWNER = "AINomi";
+export const REPO = "AINomi糯米";
+export const REPO_URL = `https://www.ainomi.cn`;
+export const ISSUE_URL = `https://www.ainomi.cn`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const RELEASE_URL = `${REPO_URL}/releases`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
