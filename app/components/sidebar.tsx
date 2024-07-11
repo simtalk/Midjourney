16351a2f9716348b41e891315d050c3fe4e94968
simtalk <55088988+simtalk@users.noreply.github.com>
2024-07-11 08:29:02 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,7 +118,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT-Midjourney
+          AINomi糯米
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Your own AI assistant.
